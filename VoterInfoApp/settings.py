727c59234069d4e8e52aebc18ef3aaad374f3598
Clay Harned <charne3@lsu.edu>
2020-03-31 17:11:05 -0500

    eh

--- VoterInfoApp/settings.py
+++ VoterInfoApp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'VoterInfoApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [(os.path.join(BASE_DIR, 'templates')), ], #new
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
