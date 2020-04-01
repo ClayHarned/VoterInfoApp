1d2b41c3b3b16e303b47520fc277c30b5e456c47
Clay Harned <charne3@lsu.edu>
2020-04-01 08:24:53 -0500

    path fixed i hope

--- VoterInfo/urls.py
+++ VoterInfo/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .views import MyInfoPageView, NextElectionPageView, PolitionProfilesPageView, CurrentSeatsPageView, VotingStatisticsPageView, VotingQuestionsAndTipsPageView
 #was originally 'about/' in first path
 urlpatterns = [
-    path('MyInfo/', MyInfoPageView.as_view(), name='MyInfo'),
-    path('NextElection/', NextElectionPageView.as_view(), name='NextElection'),
-    path('CurrentSeats/', CurrentSeatsPageView.as_view(), name='CurrentSeats'),
-    path('PolitionProfiles/', PolitionProfilesPageView.as_view(), name='PolitionProfiles'),
+    path('VotingQuestionsAndTips/', VotingQuestionsAndTipsPageView.as_view(), name='VotingQuestionsAndTips'),
     path('VotingStatistics/', VotingStatisticsPageView.as_view(), name='VotingStatistics'),
-    path('VotingQuestionsAndTips', VotingQuestionsAndTipsPageView.as_view(), name='VotingQuestionsAndTips'),
+    path('PolitionProfiles/', PolitionProfilesPageView.as_view(), name='PolitionProfiles'),
+    path('CurrentSeats/', CurrentSeatsPageView.as_view(), name='CurrentSeats'),
+    path('NextElection/', NextElectionPageView.as_view(), name='NextElection'),
+    path('', MyInfoPageView.as_view(), name='MyInfo')
 
 ]
